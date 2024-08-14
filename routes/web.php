2353cb66868caa272ed58766cfedb212a1d34369
riddhi-vh <riddhitrivedi.vhits@gmail.com>
2024-08-14 13:04:07 +0530

    routes web file changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +18,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/home', 'index')->name('home');
+});
 
-Route::get('/product/list', [App\Http\Controllers\ProductController::class, 'index'])->name('product-list');
-Route::get('/product/add', [App\Http\Controllers\ProductController::class, 'create'])->name('product-create');
-Route::get('/product/edit/{id}', [App\Http\Controllers\ProductController::class, 'edit'])->name('product-edit');
-Route::get('/product/delete/{id}', [App\Http\Controllers\ProductController::class, 'destroy'])->name('product-delete');
-Route::post('/product/save', [App\Http\Controllers\ProductController::class, 'save'])->name('product-save');
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/product/list', 'index')->name('product-list');
+    Route::get('/product/add', 'create')->name('product-create');
+    Route::post('/product/save', 'save')->name('product-save');
+    Route::get('/product/edit/{id}', 'edit')->name('product-edit');
+    Route::get('/product/delete/{id}', 'destroy')->name('product-delete');
+});
+
+// Route::get('/product/list', [App\Http\Controllers\ProductController::class, 'index'])->name('product-list');
+// Route::get('/product/add', [App\Http\Controllers\ProductController::class, 'create'])->name('product-create');
+// Route::get('/product/edit/{id}', [App\Http\Controllers\ProductController::class, 'edit'])->name('product-edit');
+// Route::get('/product/delete/{id}', [App\Http\Controllers\ProductController::class, 'destroy'])->name('product-delete');
+// Route::post('/product/save', [App\Http\Controllers\ProductController::class, 'save'])->name('product-save');
